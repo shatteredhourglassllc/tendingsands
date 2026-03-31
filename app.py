68769493348cd48e43ac23d1bb62668c09ba2eaa
shatteredhourglassllc <benchmarkauthoritymusic@hotmail.com>
2026-03-31 14:33:33 -0700

    Initialize Flask app with routes for index and music

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/music/<path:filename>")
+def music(filename):
+    return send_from_directory("music", filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
